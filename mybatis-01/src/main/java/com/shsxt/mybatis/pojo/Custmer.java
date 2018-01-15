4bf516ad95429b2436292181a14eb1c8ef5d5365
yongganzhang <1285963544@qq.com>
2018-01-15 10:12:24 +0800

    提交mybatis

---
+++ mybatis-01/src/main/java/com/shsxt/mybatis/pojo/Custmer.java
@@ -0,0 +1,46 @@
+package com.shsxt.mybatis.pojo;
+
+/**
+ * Created by Administrator on 2018/1/15.
+ */
+public class Custmer {
+
+    private int id;
+
+    private String userName;
+
+    private String balance;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Custmer{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", balance='" + balance + '\'' +
+                '}';
+    }
+}
